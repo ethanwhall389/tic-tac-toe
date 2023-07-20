161d1b2f603ca889a9e98cca4ae5750917e52171
ethanwhall389 <125170834+ethanwhall389@users.noreply.github.com>
2023-07-20 15:45:46 +0000

    Add winner and tie messages

--- script.js
+++ script.js
@@ -38,83 +38,100 @@ const GameFlow = (() => {
         playerOne = Player('X');
         playerTwo = Player('O');
 
-        round();
-        function round () {
-            // event listener loop for each button.
-            let cell = document.querySelectorAll('.cell');
-            cell.forEach( (item) => {
-                item.addEventListener('click', (event) => {
-                    //get data attributes (indexes) from clicked cell
-                    let cellIndex = item.getAttribute('data-index');
-                    //Change clicked cell marker
-                    if (whoseTurn === 'player one') {
-                        GameBoard.board[cellIndex].marker = playerOne.marker;
-                    } else {
-                        GameBoard.board[cellIndex].marker = playerTwo.marker;
-                    }
-                    //Change whose turn it is
-                    whoseTurn == 'player one' ? whoseTurn = 'player two' : whoseTurn = 'player one';
-                    DisplayController.clearDisplay()
-                    DisplayController.updateGameBoardDisplay();
-
-                    scanArrayForWin();
-                    DisplayController.updateTurnDisplay(whoseTurn);
-
-                    round();
-                })
-            })
-
-
-        }
+        round(); 
         
-        function scanArrayForWin() {
-            let possibleWins = [
-                [0, 1, 2], //Row one  
-                [3, 4, 5], //Row two  
-                [6, 7, 8], //Row THREE  
-                [0, 3, 6], //Column ONE
-                [1, 4, 7], //Column TWO
-                [2, 5, 8], //Column THREE
-                [0, 4, 8], //Diagonal ONE
-                [2, 4, 6], //Diagonal TWO
-            ]
+    }
 
-            possibleWins.forEach( (possibilityArray) => {
-                const xWins = possibilityArray.every( (cell) => {
-                    return GameBoard.board[cell].marker == 'X';
-                })
 
-                if (xWins == true) {
-                    console.log('player one won!');
-                    winner('player one');
+    function round () {
+        // event listener loop for each button.
+        let cell = document.querySelectorAll('.cell');
+        cell.forEach( (item) => {
+            item.addEventListener('click', (event) => {
+                //get data attributes (indexes) from clicked cell
+                let cellIndex = item.getAttribute('data-index');
+                //Change clicked cell marker
+                if (whoseTurn === 'player one') {
+                    GameBoard.board[cellIndex].marker = playerOne.marker;
+                } else {
+                    GameBoard.board[cellIndex].marker = playerTwo.marker;
+                }
+                //Change whose turn it is
+                whoseTurn == 'player one' ? whoseTurn = 'player two' : whoseTurn = 'player one';
+                DisplayController.clearDisplay()
+                DisplayController.updateGameBoardDisplay();
+
+                //Check if we have a winner. If so, return out of the round function.
+                let isThereAWinner = scanArrayForWin();
+                if (isThereAWinner == true) {
                     return;
                 }
-            })
-           
-           
-            possibleWins.forEach( (possibilityArray) => {
-                const oWins = possibilityArray.every( (cell) => {
-                    return GameBoard.board[cell].marker == 'O';
-                })
-
-                if (oWins == true) {
-                    console.log('player two won!');
-                    winner('player two');
+                DisplayController.updateTurnDisplay(whoseTurn);
+                currentRound ++;
+                if (currentRound == 10) {
+                    tie();
                     return;
                 }
+                
+                //Re-call the round() function to start a new round.
+                round();
             })
-        };
-        
-        // currentRound++;
-        // if (currentRound == 10) {
-        //     tie();
-        //     return;
-        // }
-
+        })
 
 
     }
+    
+    function scanArrayForWin() {
+        let possibleWins = [
+            [0, 1, 2], //Row one  
+            [3, 4, 5], //Row two  
+            [6, 7, 8], //Row THREE  
+            [0, 3, 6], //Column ONE
+            [1, 4, 7], //Column TWO
+            [2, 5, 8], //Column THREE
+            [0, 4, 8], //Diagonal ONE
+            [2, 4, 6], //Diagonal TWO
+        ]
 
+        let isThereAWinner = false;
+
+        possibleWins.forEach( (possibilityArray) => {
+            const xWins = possibilityArray.every( (cell) => {
+                return GameBoard.board[cell].marker == 'X';
+            })
+
+            if (xWins == true) {
+                isThereAWinner = true;
+                winner('player one');
+                return;
+            }
+        })
+       
+       
+        possibleWins.forEach( (possibilityArray) => {
+            const oWins = possibilityArray.every( (cell) => {
+                return GameBoard.board[cell].marker == 'O';
+            })
+
+            if (oWins == true) {
+                isThereAWinner = true;
+                winner('player two');
+                return;
+            }
+        })
+        return isThereAWinner;
+    };
+
+    function winner (winningPlayer)  {
+        console.log(`${winningPlayer} won!`);
+        DisplayController.displayWinner(winningPlayer);
+    }
+
+    function tie ()  {
+        console.log('It\'s a tie!');
+        DisplayController.displayTie();
+    }
+    
     return {newGame};
 })()
 
@@ -145,7 +162,19 @@ const DisplayController = (() => {
         currentTurnDisplay.appendChild(currentTurnPara);
     }
 
-    return {clearDisplay, updateGameBoardDisplay, updateTurnDisplay};
+    function displayWinner (winningPlayer) {
+        let winnerHeading = document.createElement('h1');
+        winnerHeading.textContent = `${winningPlayer} won!`
+        mainDisplay.appendChild(winnerHeading);
+    }
+
+    function displayTie () {
+        let TieHeading = document.createElement('h1');
+        TieHeading.textContent = 'It\'s a tie!'
+        mainDisplay.appendChild(TieHeading);
+    }
+
+    return {clearDisplay, updateGameBoardDisplay, updateTurnDisplay, displayWinner, displayTie};
 })();
 
 
